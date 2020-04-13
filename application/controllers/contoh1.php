8f7e93be1636dec615c4b94c48ed2b5130866264
Med <fikriyansah1@gmail.com>
2020-04-14 01:40:46 +0700

    memed

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+      echo "<h1>Open booking</h1>";
+      echo "Nama supri yong lex saya menjadi gigolo karena kebutuan hidup";
+    }
+}
